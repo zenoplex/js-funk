37a6b0a39183588b0cc8ea49fc5fc7713947f61b
Gen Ichino <zenoplex@gmail.com>
2020-04-11 01:13:13 +0900

    refactor: refactor code

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
-// Basics
-const basics = {
-  false: '![]+[]',
-  true: '!![]+[]',
-  undefined: '([][[]]+[])',
-  NaN: '(+{}+[])',
-  Infinity: '((+!![]/+[])+[])',
+const refs = {
+  false: '![]',
+  true: '!![]',
+  undefined: '[][[]]',
+  NaN: '+{}',
+  Infinity: '+!![]/+[]',
+  obj: '{}',
+  emptyStr: '[]+[]',
+  zero: '+[]',
+  one: '+!+[]',
+  fn: '()=>{}',
 };
 
-// Numbers
-const zero = '+[]';
-const one = '+!+[]';
+const stringify = (str: string): string => `(${str})+[]`;
 
 const fromNumber = (length: number): string =>
-  length === 0 ? zero : Array.from({ length }, () => one).join(' + ');
+  length === 0 ? refs.zero : Array.from({ length }, () => refs.one).join(' + ');
 
 const map: Record<string, string> = {};
 
@@ -26,37 +28,42 @@ const fromString = (str: string): string =>
         'fromCharCode'
       )}](${fromNumber(code)})`;
     })
-    .join(' + ');
+    .join('+');
 
 // prepare letters for `constructor`
-map.c = `({}+[])[${fromNumber(5)}]`; // [object Object];
-map.o = `({}+[])[${fromNumber(1)}]`; // [object Object]
-map.n = `(${basics.Infinity})[${fromNumber(1)}]`;
-map.s = `(${basics.false})[${fromNumber(3)}]`; // false;
-map.t = `({}+[])[${fromNumber(6)}]`; // [object Object]
-map.r = `(${basics.true})[${fromNumber(1)}]`; // true;
-map.u = `(${basics.true})[${fromNumber(2)}]`; // true;
+map.c = `(${stringify(refs.obj)})[${fromNumber(5)}]`;
+map.o = `(${stringify(refs.obj)})[${fromNumber(1)}]`;
+map.n = `(${stringify(refs.Infinity)})[${fromNumber(1)}]`;
+map.s = `(${stringify(refs.false)})[${fromNumber(3)}]`;
+map.t = `(${stringify(refs.obj)})[${fromNumber(6)}]`;
+map.r = `(${stringify(refs.true)})[${fromNumber(1)}]`;
+map.u = `(${stringify(refs.true)})[${fromNumber(2)}]`;
+
+const stringConstructor = `(${refs.emptyStr})[${fromString('constructor')}]`;
+const numberConstructor = `(${refs.one})[${fromString('constructor')}]`;
+const functionConstructor = `(${refs.fn})[${fromString('constructor')}]`;
+const regExpConstructor = `(/ /)[${fromString('constructor')}]`;
 
 // prepare letters for `toString`
-map.S = `([]+([]+[])[${fromString('constructor')}])[${fromNumber(9)}]`; // function String;
-map.g = `([]+([]+[])[${fromString('constructor')}])[${fromNumber(14)}]`; //function String
-map.i = `(${basics.Infinity})[${fromNumber(3)}]`;
+map.S = `(${stringify(stringConstructor)})[${fromNumber(9)}]`;
+map.g = `(${stringify(stringConstructor)})[${fromNumber(14)}]`;
+map.i = `(${stringify(refs.Infinity)})[${fromNumber(3)}]`;
 
 // prepare letters for `return escape`
-map.a = `(${basics.NaN})[${fromNumber(1)}]`;
-map.e = `({}+[])[${fromNumber(4)}]`; // [object Object]
-map.p = `([]+(/ /)[${fromString('constructor')}])[${fromNumber(14)}]`; // function RegExp
-map[' '] = `({}+[])[${fromNumber(7)}]`; // [object Object]
-map['\\'] = `(/\\\\/+[])[${fromNumber(1)}]`;
+map.a = `(${stringify(refs.NaN)})[${fromNumber(1)}]`;
+map.e = `(${stringify(refs.obj)})[${fromNumber(4)}]`;
+map.p = `(${stringify(regExpConstructor)})[${fromNumber(14)}]`;
+map[' '] = `(${stringify(refs.obj)})[${fromNumber(7)}]`;
+map['\\'] = `(${stringify('/\\\\/')})[${fromNumber(1)}]`;
 
 // prepare letters for String.fromCharCode
-map.f = `(${basics.false})[${fromNumber(0)}]`; // false;
-map.m = `([]+(${one})[${fromString('constructor')}])[${fromNumber(11)}]`; // function Number
-map.C = `((() => {})[${fromString('constructor')}](${fromString(
-  'return escape'
-)})()(${map['\\']}))[${fromNumber(2)}]`; // escape('\\')
-map.h = `(${fromNumber(17)})[${fromString('toString')}](${fromNumber(18)})`; // 18進数の最後の文字
-map.d = `(${basics.undefined})[${fromNumber(2)}]`;
+map.f = `(${stringify(refs.false)})[${fromNumber(0)}]`;
+map.m = `(${stringify(numberConstructor)})[${fromNumber(11)}]`;
+map.C = `(${functionConstructor}(${fromString('return escape')})()(${
+  map['\\']
+}))[${fromNumber(2)}]`;
+map.h = `(${fromNumber(17)})[${fromString('toString')}](${fromNumber(18)})`;
+map.d = `(${stringify(refs.undefined)})[${fromNumber(2)}]`;
 
 export const encode = (code: string): string =>
-  `(()=>{})[${fromString('constructor')}](${fromString(code)})()`;
+  `${functionConstructor}(${fromString(code)})()`;
