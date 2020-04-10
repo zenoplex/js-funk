f28b62a4a62421ba9616c43a7bf6ef28e5abb860
Gen Ichino <zenoplex@gmail.com>
2020-04-10 23:51:13 +0900

    docs: update comments

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,6 @@ const one = '+!+[]';
 const fromNumber = (length: number): string =>
   length === 0 ? zero : Array.from({ length }, () => one).join(' + ');
 
-// acdefghimnoprstuCS[space][backslash]
-
 const map: Record<string, string> = {};
 
 const fromString = (str: string): string =>
@@ -30,7 +28,7 @@ const fromString = (str: string): string =>
     })
     .join(' + ');
 
-// constructor のために用意
+// prepare letters for `constructor`
 map.c = `({}+[])[${fromNumber(5)}]`; // [object Object];
 map.o = `({}+[])[${fromNumber(1)}]`; // [object Object]
 map.n = `(${basics.Infinity})[${fromNumber(1)}]`;
@@ -39,19 +37,19 @@ map.t = `({}+[])[${fromNumber(6)}]`; // [object Object]
 map.r = `(${basics.true})[${fromNumber(1)}]`; // true;
 map.u = `(${basics.true})[${fromNumber(2)}]`; // true;
 
-// toString のために用意
+// prepare letters for `toString`
 map.S = `([]+([]+[])[${fromString('constructor')}])[${fromNumber(9)}]`; // function String;
 map.g = `([]+([]+[])[${fromString('constructor')}])[${fromNumber(14)}]`; //function String
 map.i = `(${basics.Infinity})[${fromNumber(3)}]`;
 
-// return escape のために用意
+// prepare letters for `return escape`
 map.a = `(${basics.NaN})[${fromNumber(1)}]`;
 map.e = `({}+[])[${fromNumber(4)}]`; // [object Object]
 map.p = `([]+(/ /)[${fromString('constructor')}])[${fromNumber(14)}]`; // function RegExp
 map[' '] = `({}+[])[${fromNumber(7)}]`; // [object Object]
 map['\\'] = `(/\\\\/+[])[${fromNumber(1)}]`;
 
-//fromCharCode
+// prepare letters for String.fromCharCode
 map.f = `(${basics.false})[${fromNumber(0)}]`; // false;
 map.m = `([]+(${one})[${fromString('constructor')}])[${fromNumber(11)}]`; // function Number
 map.C = `((() => {})[${fromString('constructor')}](${fromString(
@@ -60,9 +58,5 @@ map.C = `((() => {})[${fromString('constructor')}](${fromString(
 map.h = `(${fromNumber(17)})[${fromString('toString')}](${fromNumber(18)})`; // 18進数の最後の文字
 map.d = `(${basics.undefined})[${fromNumber(2)}]`;
 
-const encode = (code: string): string =>
+export const encode = (code: string): string =>
   `(()=>{})[${fromString('constructor')}](${fromString(code)})()`;
-
-console.log(encode('console.log("do me well")'));
-
-// Object.keys(map).map(key => console.log(key, eval(map[key])));
